57b508f511a31019c15fd904e372ce17990dfc57
Gábor Borbély <gaebor@gmail.com>
2021-12-27 15:59:08 +0100

    kind-of-working

--- waterfall.py
+++ waterfall.py
@@ -1,26 +1,94 @@
 import json
+import argparse
+import re
 
-import tornado.options
+from termcolor import colored
+
 import tornado.ioloop
 import tornado.gen
 from tornado.websocket import websocket_connect
 
 
-@tornado.gen.coroutine
-def run(url):
-    connection = yield websocket_connect(url)
-    while True:
-        msg = yield connection.read_message()
-        if msg is None:
-            break
-        print(json.loads(msg))
+class ParseKwargs(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, dict())
+        for value in values:
+            key, value = value.split('=')
+            getattr(namespace, self.dest)[key] = int(value)
+
+
+def text_cap(s, width):
+    if width == 0:
+        return ''
+    return f'{s[:width]:{width}s}'
+
+
+def print_bar(text, highlight1_width, highlight2_width=0, total_width=80):
+    total_width = int(max(0, total_width))
+    highlight1_width = int(min(highlight1_width, total_width))
+    highlight2_width = int(min(highlight2_width, total_width - highlight1_width))
+    part1 = (
+        colored(text_cap(text, highlight1_width), attrs=['reverse'])
+        if highlight1_width > 0
+        else ''
+    )
+    part2 = (
+        colored(
+            text_cap(text[highlight1_width:], highlight2_width),
+            on_color='on_red',
+        )
+        if highlight2_width > 0
+        else ''
+    )
+    part3 = text_cap(
+        text[highlight1_width + highlight2_width :],
+        total_width - (highlight1_width + highlight2_width),
+    )
+
+    print(part1 + part2 + part3, end='')
+
+
+def print_line(message, factors, width=20):
+    for pattern, factor in factors.items():
+        for values in message:
+            if re.match(pattern, values[0]) is not None:
+                text = values[0]
+                if len(values) > 3:
+                    text = values[3]
+                print_bar(
+                    text, width * values[1] / factor, width * values[2] / factor, total_width=width
+                )
+    print('', flush=True)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--url', default='ws://localhost:8888/waterfall', help=' ')
+    parser.add_argument('--width', default=20, help=' ', type=int)
+
+    parser.add_argument(
+        '--factors',
+        nargs='+',
+        action=ParseKwargs,
+        help='`item=factor` pairs',
+        default={'cpu\\d+': 100, 'memory': 100},
+    )
+    return parser.parse_args()
 
 
 def main():
-    tornado.options.define("url", default='ws://localhost:8888/waterfall', type=str)
-    tornado.options.parse_command_line()
+    args = get_args()
 
-    run(tornado.options.options.url)
+    @tornado.gen.coroutine
+    def run():
+        connection = yield websocket_connect(args.url)
+        while True:
+            msg = yield connection.read_message()
+            if msg is None:
+                break
+            print_line(json.loads(msg), args.factors, width=args.width)
+
+    run()
     tornado.ioloop.IOLoop.current().start()
 
 
