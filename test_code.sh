3e227184ed5ea7d9d9355ea53133b49b38d68cb5
Gábor Borbély <gaebor@gmail.com>
2022-11-12 15:32:34 +0100

    try timeout cleanly

--- test_code.sh
+++ test_code.sh
@@ -8,5 +8,5 @@ python -m pip install .[webserver]
 python -m waterfall.server -h
 python -m waterfall.client -h
 
-timeout 20 python -m waterfall.server &
-timeout 10 python -m waterfall.client
+timeout --preserve-status 20 python -m waterfall.server &
+timeout --preserve-status 10 python -m waterfall.client
