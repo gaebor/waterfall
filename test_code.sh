92cbe403e204ccdd08b4755e26fc6fd0c68d1193
Gábor Borbély <gaebor@gmail.com>
2022-11-12 14:46:56 +0100

    separate tests

--- test_code.sh
+++ test_code.sh
@@ -1,9 +1,10 @@
-dir
+pwd
+ls
 python -m pip install .
 python -m waterfall -h
 python -m waterfall -n 2
 
 python -m pip install .[webserver]
 
-start /b python -m waterfall.server
+python -m waterfall.server &
 python -m waterfall.client
