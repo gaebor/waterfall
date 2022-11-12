fb3b90e2843e4998bca174c9b237506831e19415
Gábor Borbély <gaebor@gmail.com>
2022-11-12 16:16:13 +0100

    windows script kind-of-works

--- check_code.cmd
+++
@@ -1,2 +0,0 @@
-black --check --line-length 99 --skip-string-normalization setup.py waterfall
-pylint --min-public-methods=1 --disable=C0114,C0115,C0116 --fail-under 10 --max-line-length 99 setup.py waterfall
--- test_code.cmd
+++ test_code.cmd
@@ -1,9 +1,17 @@
 python -m pip install .
+IF %ERRORLEVEL% NEQ 0 (exit 1)
+
 python -m waterfall -h
+IF %ERRORLEVEL% NEQ 0 (exit 1)
 python -m waterfall -n 2
+IF %ERRORLEVEL% NEQ 0 (exit 1)
 python -m waterfall -n 2 --descriptors "^^cpu0" 10 "cpu$" memory 100 20 --width 10
+IF %ERRORLEVEL% NEQ 0 (exit 1)
 
 python -m pip install .[webserver]
+IF %ERRORLEVEL% NEQ 0 (exit 1)
 
 python -m waterfall.server -h
+IF %ERRORLEVEL% NEQ 0 (exit 1)
 python -m waterfall.client -h
+IF %ERRORLEVEL% NEQ 0 (exit 1)
