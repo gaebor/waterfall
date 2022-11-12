f567ace373507c103828de63b3697b2476aa0a3c
Gábor Borbély <gaebor@gmail.com>
2022-11-12 16:07:12 +0100

    try cmd script

--- test_code.cmd
+++ test_code.cmd
@@ -1,11 +1,9 @@
-dir
 python -m pip install .
 python -m waterfall -h
 python -m waterfall -n 2
 python -m waterfall -n 2 --descriptors "^^cpu0" 10 "cpu$" memory 100 20 --width 10
 
-
 python -m pip install .[webserver]
 
-start /b python -m waterfall.server
-python -m waterfall.client
+python -m waterfall.server -h
+python -m waterfall.client -h
