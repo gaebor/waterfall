1aaa39df0ef07ce6becf5bc25552fdab42cc4fbb
Gábor Borbély <gaebor@gmail.com>
2022-11-12 09:46:00 +0100

    remove crontab stuff

--- setup.py
+++ setup.py
@@ -1,25 +1,5 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
-from setuptools.command.install import install
-from re import search
-from sys import executable
-
-
-class RegisterCrontab(install):
-    def run(self):
-        install.run(self)
-        name = self.distribution.get_name()
-        arguments = self.distribution.script_args
-        is_webserver_startup = list(
-            filter(bool, (search('\[webserver_startup\]$', argument) for argument in arguments))
-        )
-        if is_webserver_startup:
-            from crontab import CronTab
-
-            with CronTab(user=True) as cron:
-                job = cron.new(command=f'"{executable}" -m waterfall.server')
-                job.every_reboot()
-
 
 setup(
     name='waterfall',
@@ -30,7 +10,6 @@ setup(
     url='https://github.com/gaebor/waterfall',
     license='MIT',
     install_requires=['termcolor', 'psutil'],
-    extras_require={'webserver': ['tornado'], 'webserver_startup': ['tornado', 'python-crontab']},
+    extras_require={'webserver': ['tornado']},
     packages=['waterfall'],
-    cmdclass={'install': RegisterCrontab},
 )
