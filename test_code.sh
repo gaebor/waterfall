746a4e2a930be58d68f6c2e01f9a3e5a891a8215
Gábor Borbély <gaebor@gmail.com>
2022-11-12 14:53:55 +0100

    try

--- test_code.sh
+++ test_code.sh
@@ -1,10 +1,9 @@
-pwd
-ls
+set -e
 python -m pip install .
 python -m waterfall -h
 python -m waterfall -n 2
 
 python -m pip install .[webserver]
 
-python -m waterfall.server &
-python -m waterfall.client
+python -m waterfall.server -h
+python -m waterfall.client -h
